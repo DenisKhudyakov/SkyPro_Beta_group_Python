e2c241b36b1b3fbf40584af2081482b8573cb1c8
Denis Khudyakov <denis.hudyakov27@gamil.com>
2023-11-03 11:23:52 +0500

    editing generators.py

--- src/generators.py
+++ src/generators.py
@@ -1,8 +1,7 @@
-import json
-from typing import Any, Callable
+from typing import Any, Generator
 
 
-def filter_by_currency(bank_obj: Any, currency: str) -> Callable:
+def filter_by_currency(bank_obj: Any, currency: str) -> Generator:
     """
     Функиция итератор, фильтрующая банковские операции по заданной валюте
     :param bank_obj: Any
@@ -17,7 +16,7 @@ def filter_by_currency(bank_obj: Any, currency: str) -> Callable:
             yield money_type
 
 
-def transaction_descriptions(trans_list: list) -> Callable:
+def transaction_descriptions(trans_list: list) -> Generator:
     """
     генератор, который принимает список словарей и возвращает описание каждой операции по очереди.
     :param trans_list:
@@ -27,14 +26,17 @@ def transaction_descriptions(trans_list: list) -> Callable:
         yield operation["description"]
 
 
-def card_number_generator(start: int, end: int) -> Callable:
+def card_number_generator(start: int, end: int) -> Generator:
     """
     Генератор номеров кард
     :param start: inr
     :param end: inr
     :return: generator
     """
-    number = "0000000000000000"
-    for i in range(start, end + 1):
-        number_card = number[0 : -len(str(i))] + str(i)
-        yield " ".join([number_card[0:4], number_card[4:8], number_card[8:12], number_card[12:16]])
+    number_card_ver2 = ((16*'0')[0:-len(str(number))]+str(number) for number in range(1, 5+1))
+    # number = "0000000000000000"
+    # for i in range(start, end + 1):
+    #     number_card = number[0 : -len(str(i))] + str(i)
+    #     yield " ".join([number_card[0:4], number_card[4:8], number_card[8:12], number_card[12:16]])
+    for one_number in number_card_ver2:
+        yield one_number
