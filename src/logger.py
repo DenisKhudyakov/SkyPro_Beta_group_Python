4494cbebf5f8ad3bbcdf682ad687433f6091d797
Denis Khudyakov <denis.hudyakov27@gamil.com>
2023-11-17 20:15:58 +0500

    add docstring

--- src/logger.py
+++ src/logger.py
@@ -3,6 +3,10 @@ from typing import Any
 
 
 def setup_logging() -> Any:
+    """
+    Функция записи информации в файл
+    :return: логи информации
+    """
     logger = logging.getLogger(__name__)
     file_handler = logging.FileHandler("data_collection.log", "w")
     file_formatter = logging.Formatter("%(asctime)s %(funcName)s %(levelname)s: %(message)s")
@@ -13,6 +17,10 @@ def setup_logging() -> Any:
 
 
 def setup_error_logging() -> Any:
+    """
+    Функция записи ошибок в файл
+    :return: логи ошибок
+    """
     logger = logging.getLogger(__name__)
     file_handler = logging.FileHandler("data_collection.log", "w")
     file_formatter = logging.Formatter("%(asctime)s %(funcName)s %(levelname)s: %(message)s")
