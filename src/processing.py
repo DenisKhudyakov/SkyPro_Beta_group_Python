52acffd46e04951b78715653a3724db361b46b01
Denis Khudyakov <denis.hudyakov27@gamil.com>
2023-11-22 21:35:28 +0500

    operation_search done

--- src/processing.py
+++ src/processing.py
@@ -1,5 +1,7 @@
 from typing import Any
-
+import re
+from src.read_the_table import read_xlsx_csv_file
+from data.config import PATH_CSV_FILE
 
 def filter_operations(operation_list: list[dict[Any, Any]], state_value: str = "EXECUTED") -> list[Any] | None:
     """
@@ -28,3 +30,27 @@ def sorted_operation(operation_list: list[dict[Any, Any]], revers: bool = False)
         :return: возвращаем список отсортированный по дате
     """
     return sorted(operation_list, key=lambda date: date["date"], reverse=revers) if operation_list else None
+
+def operation_search(data_bank: Any, search_word: str) -> list:
+    """
+    Поисковой движок банковских операций
+    :param data_bank: банковские операции
+    :param search_word: искомое слово
+    :return: новый список
+    """
+    def is_search(word: str, any_object: Any):
+        """
+        Булевая функция на проверку вхождения слова в объект
+        :param word: искомое слово
+        :param any_object: любой объект
+        :return: булевая переменная
+        """
+        match = re.search(word, str(any_object))
+        return True if match else False
+    return list(filter(lambda x: is_search(search_word, x.values()), list(data_bank)))
+
+
+def operation_counter(data_bank: list, operation: dict) -> dict:
+    pass
+if __name__ == '__main__':
+    print(operation_search(read_xlsx_csv_file(PATH_CSV_FILE), '2023-01-04T13:13:34Z'))
--- src/read_the_table.py
+++ src/read_the_table.py
@@ -4,36 +4,35 @@ import pandas as pd
 
 from data.config import PATH_CSV_FILE, PATH_XLSX_FILE
 
-one_transactions = {
-    "id": None,
-    "state": None,
-    "date": None,
-    "operationAmount": {"amount": None, "currency": {"name": None, "code": None}},
-    "description": None,
-    "from": None,
-    "to": None,
-}
 
 
-def transformation_of_the_structure(list_: list, struct: dict) -> list:
+
+def transformation_of_the_structure(list_: list) -> list:
     """
     Функция преобразования входного списка в словарь заданной структуры
     :param list_: список с банковскими данными
     :param struct: необходимая структура словаря
     :return: возвращемое значение список со словарём
     """
-    transactions_list = []
-    struct["id"] = str(list_[0])[:-2]
-    struct["state"] = list_[1]
-    struct["date"] = list_[2]
-    struct["operationAmount"]["amount"] = list_[3]
-    struct["operationAmount"]["currency"]["name"] = list_[4]
-    struct["operationAmount"]["currency"]["code"] = list_[5]
-    struct["description"] = list_[8]
-    struct["from"] = list_[6]
-    struct["to"] = list_[7]
-    transactions_list.append(struct)
-    return transactions_list
+    one_transactions = {
+        "id": None,
+        "state": None,
+        "date": None,
+        "operationAmount": {"amount": None, "currency": {"name": None, "code": None}},
+        "description": None,
+        "from": None,
+        "to": None,
+    }
+    one_transactions["id"] = str(list_[0])[:-2]
+    one_transactions["state"] = list_[1]
+    one_transactions["date"] = list_[2]
+    one_transactions["operationAmount"]["amount"] = list_[3]
+    one_transactions["operationAmount"]["currency"]["name"] = list_[4]
+    one_transactions["operationAmount"]["currency"]["code"] = list_[5]
+    one_transactions["description"] = list_[8]
+    one_transactions["from"] = list_[6]
+    one_transactions["to"] = list_[7]
+    return one_transactions
 
 
 def read_xlsx_csv_file(file: Any) -> Any:
@@ -45,11 +44,13 @@ def read_xlsx_csv_file(file: Any) -> Any:
     if str(file).endswith(".csv"):
         df = pd.read_csv(file, delimiter=";")
         for row in df.iterrows():
-            yield transformation_of_the_structure(row[1].tolist(), one_transactions)
-    df = pd.read_excel(file)
-    for row in df.iterrows():
-        yield transformation_of_the_structure(row[1].tolist(), one_transactions)
+            yield transformation_of_the_structure(row[1].tolist())
+    else:
+        df = pd.read_excel(file)
+        for row in df.iterrows():
+            yield transformation_of_the_structure(row[1].tolist())
 
+if __name__ == '__main__':
+    # print(next(read_xlsx_csv_file(PATH_XLSX_FILE)))
+    print(list(read_xlsx_csv_file(PATH_CSV_FILE)))
 
-print(next(read_xlsx_csv_file(PATH_XLSX_FILE)))
-print(next(read_xlsx_csv_file(PATH_CSV_FILE)))
